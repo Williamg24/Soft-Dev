177e4350291a78e2dacd03825b568f8f75482a9b
William Guo <wguo30@stuy.edu>
2022-10-03 22:01:50 -0400

    fixed error when copied over code from teammate

---
+++ 06_py-csv/numbercruncher.py
@@ -0,0 +1,60 @@
+""" 
+Team Don't Worry
+David Chen, William Guo
+SoftDev
+06_py-csv
+2022-09-29
+time spent: 0.7 hours
+
+DISCO: 
+    - You don't need to import io to read files
+    - You can pass weights into random.choices
+QCC:
+    - N/A
+
+"""
+from operator import indexOf
+import random
+import math
+
+with open("./occupations.csv", 'r', encoding='utf8') as f:
+    occupation_data = f.read()
+    f.close()
+
+# initialize variables
+occupation_list = occupation_data.split("\n")
+occupation_classes = []
+occupation_percentages = []
+
+def comma_in_field(row):
+    data = row.split('"')
+    # print(data)
+    percentage = row.split(",") 
+    #print(percentage[-1])
+    return [data[1], percentage[-1]]
+
+for occupation in occupation_list:
+    if '"' in occupation:
+        data_in_comma = comma_in_field(occupation)
+        occupation_classes.append(data_in_comma[0])
+        occupation_percentages.append(float(data_in_comma[1]))
+    else:
+        data = occupation.split(",")
+        occupation_classes.append(data[0])
+        occupation_percentages.append((data[1]))
+
+# remove the labels
+occupation_classes.pop(0)
+occupation_percentages.pop(0)
+occupation_classes.pop(len(occupation_classes)-1)
+occupation_percentages.pop(len(occupation_percentages)-1)
+
+
+# print(occupation_classes)
+# print(len(occupation_classes))
+# print(occupation_percentages)
+# print(len(occupation_percentages))
+
+
+# convert list of string for percentages to list of floats
+print(random.choices(occupation_classes,weights=(list(map(float,occupation_percentages)))))
