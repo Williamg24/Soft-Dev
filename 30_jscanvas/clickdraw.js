85858b3a4c71d8b32bdb9d67cc680a3a80427a78
William Guo <wguo30@stuy.edu>
2023-04-24 12:28:56 -0400

    weeeeee done with 30

--- 30_jscanvas/clickdraw.js
+++ 30_jscanvas/clickdraw.js
@@ -12,31 +12,52 @@ var toggleMode = (e) => {
     console.log("toggling...");
     if(mode == "rect"){
         mode = "circ"
+        bToggler.innerHTML = "Circle"
     }
     else{
         mode = "rect"
+        bToggler.innerHTML = "Rect"
     }
 }
 
 var drawRect = function(e) {
-    var mouseX = e.clientX;
-    var mouseY = e.clientY;
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
+    
+    ctx.beginPath()
+    ctx.fillStyle = 'orange'
+    ctx.strokeRect(mouseX, mouseY,20,50)
+    ctx.fillRect(mouseX, mouseY,20,50)
+    ctx.stroke()
 }
 
 //var drawCircle = function(e) {
 var drawCircle = (e) => {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
     console.log("mouseclick registered at ", mouseX, mouseY);
+
+    ctx.beginPath()
+    ctx.fillStyle = 'orange'
+    ctx.arc(mouseX, mouseY, 15, 0, 2 * Math.PI, false)
+    ctx.fill()
+    ctx.stroke()
 }
 
 //var draw = function(){
 var draw = (e) => {
-
+    if (mode == "rect"){
+        drawRect(e)
+    }
+    else{
+        drawCircle(e)
+    }
 }
 
 //var wipeCanvas = function(){
 var wipeCanvas = () => {
-
+    ctx.clearRect(0,0,600,600);
 }
 
 c.addEventListener("click", draw);
